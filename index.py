e142584cc625490994a5fa24fdda40572bf8558b
zoe12378 <144758574+zoe12378@users.noreply.github.com>
2023-12-22 16:24:29 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 def index():
     homepage = "<br><a href=/webhook3>3</a>"
     homepage += "<br><a href=/webhook4>4</a>"
-    homepage += "<br><a href=/webhook5>z5</a>"
+    homepage += "<br><a href=/webhook5>5</a>"
     return homepage
 
 @app.route("/webhook3", methods=["POST"])
@@ -117,7 +117,7 @@ def webhook5():
                     found = True 
                     info += "課程名稱：" + dict["title"] + "\n"
                     info += "開課單位：" + dict["owner_name"] + "\n"
-                    info += "價錢：" + dict["price"] + "\n"
+                    info += "價錢：" + str(dict["price"]) + "\n"
                     info += "開課人數: " + str(dict["student_number"]) + "\n\n"
             if not found:
                 info += "很抱歉，目前無符合這個關鍵字的相關課程喔"

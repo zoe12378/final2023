dc8303264566f9a9106d00febe6e8cf5ef5911dd
zoe12378 <144758574+zoe12378@users.noreply.github.com>
2023-12-22 15:41:43 +0800

    Add files via upload

--- index.py
+++ index.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     homepage = "<br><a href=/webhook3>zzzz3</a>"
-    homepage += "<br><a href=/webhook4>zzzzz4</a>"
+    homepage += "<br><a href=/webhook4>4</a>"
     homepage += "<br><a href=/webhook5>5</a>"
     return homepage
 
@@ -66,7 +66,7 @@ def webhook4():
                 result += "課程名稱：" + dict["title"] + "\n"
                 result += "開課單位：" + dict["owner_name"] + "\n"
                 result += "開課人數: " + dict["student_number"] + "\n\n"
-        # info += result
+        info += result
     #開一個Intent
     #action 叫 hahowclass
     elif (action == "hahowclass"):
